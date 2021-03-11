50601b176dce3215d4c0338122506aaa2f0af3f1
MaxiPineker <maxi.pineker@gmail.com>
2021-03-11 11:40:21 +0100

    database access

---
+++ FriendsAadPicker/FriendsAadPicker/Data/Friend.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FriendsAadPicker.Data
+{
+    public class Friend
+    {
+        public int ID { get; set; }
+        public int AADID { get; set; }
+    }
+}
--- FriendsAadPicker/FriendsAadPicker/Startup.cs
+++ FriendsAadPicker/FriendsAadPicker/Startup.cs
@@ -2,10 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FriendsAadPicker.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -30,6 +32,10 @@ namespace FriendsAadPicker
             services.AddMicrosoftIdentityWebApiAuthentication(Configuration);
             services.AddAuthentication();
             services.AddAuthorization();
+
+            services.AddDbContext<FriendsDbContext>(options => options.UseSqlServer(
+             Configuration["ConnectionStrings:DefaultConnection"]));
+
             services.AddControllers();
             //services.AddSwaggerGen(c =>
             //{
